abfa9d0a005526bada09dda0c3e113242c03795a
GWilczyk <gwilczyk.github@gmail.com>
2020-06-21 00:26:22 +0200

    Style Color Slider

--- src/Palette.js
+++ src/Palette.js
@@ -23,14 +23,15 @@ class Palette extends Component {
     ));
     return (
       <div className='Palette'>
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
-        {/* TODO: Navbar goes here */}
+        <div className='slider'>
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
+        </div>
         <div className='Palette-colors'>{colorBoxes}</div>
         {/* TODO: Footer */}
       </div>
