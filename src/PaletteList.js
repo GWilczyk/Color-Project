5e3c8955fac7365b5206aba9a3da8bc5653d7216
GWilczyk <gwilczyk.github@gmail.com>
2020-06-24 15:53:46 +0200

    Add withStyles with demo styles

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 class PaletteList extends Component {
   render() {
@@ -7,6 +8,7 @@ class PaletteList extends Component {
     return (
       <div>
         <h1>React Colors</h1>
+        <MiniPalette />
         {palettes.map(palette => (
           <p key={palette.id}>
             <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
