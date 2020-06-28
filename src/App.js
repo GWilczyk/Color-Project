b39ab59fc1332cf930e66baf0e32371dc76be3a5
GWilczyk <gwilczyk.github@gmail.com>
2020-06-28 14:12:20 +0200

    Add Navbar and Footer to SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ import seedColors from './seedColors.js';
 
 class App extends Component {
   findPalette(id) {
-    return seedColors.find(function (palette) {
-      return palette.id === id;
-    });
+    return seedColors.find(palette => palette.id === id);
   }
   render() {
     return (
